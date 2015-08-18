b12f39950c1e2d326c37221960c927666c939395
erantzeri <erantzeri@gmail.com>
2015-08-18 15:26:19 -0700

    Add Modify/Delete Contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -23,12 +23,23 @@ class ContactsController < ApplicationController
   end
   
   def edit
+    @contact = Contact.find(params[:id])
   end
   
   def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(params[:contact].permit(:first_name, :last_name, :children, :significant_other, :bday))
+      redirect_to @contact
+    else
+      render 'edit'
+    end 
   end
   
   def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    
+    redirect_to contacts_path
   end
   
   private
